251509726e916202e1c81cd9904987c818cad95b
jimmygso1211-1211 <jimmygso1211@gmail.com>
2026-07-04 18:31:02 -0500

    v3: SW limpia cache y pasa a red

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,9 @@
-const CACHE_NAME = 'jimmy-financiero-v2';
-const FILES_TO_CACHE = [
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
+// Service worker vaciado - la app usa localStorage, no necesita cache
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
   self.clients.claim();
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request).then((response) => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          if (event.request.method === 'GET' && response.status === 200) {
-            cache.put(event.request, response.clone());
-          }
-          return response;
-        });
-      }).catch(() => cached);
-    })
-  );
-});
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
